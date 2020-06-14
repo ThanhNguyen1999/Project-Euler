1c2d4b3f0aca62be42b5ab04c992358cb2c53c82
ThanhNguyen1999 <58368136+ThanhNguyen1999@users.noreply.github.com>
2020-06-14 14:07:05 +0700

    Add files via upload

---
+++ Hoan_doi_giatri_2_so.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int a = 4;
+	int b = 5;
+	 int x ;
+	 
+	 x = a;
+	 a= b;
+	 b=x ;
+	 
+	 //hoac 
+//	  a = a+b;
+//	  b = a-b;
+//	  a = a-b;
+	 cout << a <<endl;
+	 cout << b ;
+
+	return 0;
+}
