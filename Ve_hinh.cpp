1b49ceff2c5e8923c4277f88376300c066cf996e
ThanhNguyen1999 <58368136+ThanhNguyen1999@users.noreply.github.com>
2020-06-15 14:48:27 +0700

    Add files via upload

---
+++ Ve_hinh.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n ;
+	cin >> n;
+	
+	//bai 1
+//	for(int i = 1 ; i<=n ; i++)
+//	{
+//		for (int j = 0; j<i; j++)
+//		{
+//			
+//			cout<<"*";
+//			
+//		}
+//		cout<<endl;
+//	}
+	
+	//bai 2
+
+//	for(int i =n ; i>=1 ; i--)
+//	{
+//			for (int j = 0; j<i; j++)
+//		{
+//			
+//			cout<<"*";
+//			
+//		}
+//		cout<<endl;
+//	}
+	
+	//bai 3
+
+	
+//	for(int i =n ; i>=1 ; i--)
+//	{
+//		for (int j = 1; j<i; j++)
+//		{	
+//			cout<<" ";
+//		}
+//		for (int j=i ; j<n ;j++)
+//		{
+//			cout<<"*";
+//		}
+//		
+//		cout<<endl;
+//	}
+//			
+//	bai 3
+
+		for(int i = 1 ; i<=n ; i++)
+	{
+		for (int j = i; j<n; j++)
+		{
+			
+			cout<<"-";	
+		}
+		for (int j = 1; j<=2*i-1; j++)
+		{	
+			cout<<"*";	
+		}
+		
+		
+		cout<<endl;
+	}
+	
+	
+	
+	
+
+	
+}
