430740f4dc6c89ccad885b76fe8c4db55ea4e474
ThanhNguyen1999 <58368136+ThanhNguyen1999@users.noreply.github.com>
2020-06-15 21:33:11 +0700

    Update Ve_hinh.cpp

--- Ve_hinh.cpp
+++ Ve_hinh.cpp
@@ -30,23 +30,9 @@ int main()
 //		cout<<endl;
 //	}
 	
-	//bai 3
 
 	
-//	for(int i =n ; i>=1 ; i--)
-//	{
-//		for (int j = 1; j<i; j++)
-//		{	
-//			cout<<" ";
-//		}
-//		for (int j=i ; j<n ;j++)
-//		{
-//			cout<<"*";
-//		}
-//		
-//		cout<<endl;
-//	}
-//			
+		
 //	bai 3
 
 		for(int i = 1 ; i<=n ; i++)
@@ -66,8 +52,7 @@ int main()
 	}
 	
 	
-	
-	
-
-	
+	system("pause");
+	return 0;
+		
 }
